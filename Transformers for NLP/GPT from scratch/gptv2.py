b3db6a6a72d796edd7db270b07f586869b49a057
Alvin Kimata <alvinkimata@gmail.com>
2023-01-20 12:17:40 +0300

    add the decoder block

--- Transformers for NLP/GPT from scratch/gptv2.py
+++ Transformers for NLP/GPT from scratch/gptv2.py
@@ -123,9 +123,12 @@ class MultiHeadAttention(nn.Module):
   def __init__(self, num_heads, head_size):
     super().__init__()
     self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+    self.proj = nn.Linear(num_heads * head_size, n_embd)
 
   def forward(self, x):
-    return torch.cat([h(x) for h in self.heads], dim = -1)
+    out = torch.cat([h(x) for h in self.heads], dim = -1)
+    out = self.proj(out)
+    return out
 
 
 #Create a feed-forward layer.
@@ -134,12 +137,29 @@ class FeedFoward(nn.Module):
   def __init__(self, n_embd):
     super().__init__()
     self.net = nn.Sequential(
-      nn.Linear(n_embd, n_embd),
-      nn.ReLU()
+      nn.Linear(n_embd, 4 * n_embd),
+      nn.ReLU(),
+      nn.Linear(4 * n_embd, n_embd)
     )
   
   def forward(self, x):
     return self.net(x)
+
+#Create the decoder block.
+class Block(nn.Module):
+  '''Transformer block: Communication followed by computation'''
+
+  def __init__(self, n_embd, n_head):
+    super().__init__()
+    head_size = n_embd // n_head
+    self.sa = MultiHeadAttention(n_head, head_size)
+    self.ffwd = FeedFoward(n_embd)
+
+  def forward(self, x):
+    x = x + self.sa(x)
+    x = x + self.ffwd(x)
+    return x
+
 
 #Create a model.
 class BigramLanguageModel(nn.Module):
@@ -150,22 +170,21 @@ class BigramLanguageModel(nn.Module):
     self.token_embedding_table = nn.Embedding(vocab_size, n_embd) #number of embedding dimensions.
     self.position_embedding_table = nn.Embedding(block_size, n_embd) #number of embedding dimensions.
 
-    #4 communication channels of 8-dimensional self-attention.
-    self.sa_heads = MultiHeadAttention(4, head_size = n_embd // 4)
-    self.ffwd(FeedFoward(n_embd))
+    #Add decoder blocks.
+    self.blocks = nn.Sequential(
+      Block(n_embd, n_head = 4),
+      Block(n_embd, n_head = 4),
+      Block(n_embd, n_head = 4)
+    )
     self.lm_head = nn.Linear(n_embd, vocab_size)
+
 
   def forward(self, idx, targets = None):
     B, T = idx.shape
     tok_emb = self.token_embedding_table(idx) #(batch, time, channel)
     pos_emb = self.position_embedding_table(torch.arange(T, device = device)) #(time, channel)
     x = tok_emb + pos_emb #Broadcasting.(Batch, time, channel)
-
-    #Plug in the self-attention layer.
-    x = self.sa_heads(x) #Apply one head of self-attention.
-    
-    #Apply the feed-forward layer.
-    x = self.ffwd(x) 
+    x = self.blocks(x) 
     logits = self.lm_head(x) # (batch, time, vocab_size)
 
     if targets is None:
