1d0a0b3bdcf3d472f4e5de4c26b641ca10488362
Alvin Kimata <66922806+AlvinKimata@users.noreply.github.com>
2023-03-30 16:59:28 +0300

    Update utils.py

--- Transformers_for_NLP/document_summarization/utils.py
+++ Transformers_for_NLP/document_summarization/utils.py
@@ -34,9 +34,10 @@ def showPaperSummary(api_key, text):
 
     response = openai.ChatCompletion.create(
         engine="gpt-3.5-turbo",
-        prompt="Summarize the text below for a second-grade student:\n \n" + text,
+        prompt=,
+        messages=[{"role": "user", "content": "Summarize the text below for a second-grade student:\n \n" + text}]
         temperature = 0.8,
-        max_tokens= 200,
+        max_tokens= 300,
         top_p=1,
         frequency_penalty=0,
         presence_penalty=0,
